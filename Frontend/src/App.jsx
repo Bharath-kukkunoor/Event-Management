dc44824b2d84c9e35a25caa63eecfd63fd93578c
Bharath-kukkunoor <bharathkukkunoor@gmail.com>
2024-06-21 15:50:57 +0530

    import name error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
-import Services from "./components/Services";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import HeroSection from "./Components/HeroSection";
+import Services from "./Components/Services";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
